19c55a8eaaf3126cad14c68a61aee0b598e38d9f
Borbala Tasnadi <borbala.tasnadi@gmail.com>
2024-02-08 14:38:26 +0100

    rename pnm module to pneumonia in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
 setup(
-    name='pnm',
+    name='pneumonia',
     version='0.1.0',
     url='https://github.com/borbalita/public/medical/pneumonia',
     author='Borbala Tasnadi',
